d92b91a6df78ba01f84cdba2766f9adc852267b3
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2024-09-02 21:48:47 -0500

    009

---
+++ js/script.js
@@ -0,0 +1,43 @@
+alert("¡ despierta... ya es hora !");
+
+/*function getTime() {
+ let date = new Date();
+ let hours = date.getHours();
+ let minutes = date.getMinutes();
+ let seconds = date.getSeconds();
+ let ampm = hours >= 12 ? 'PM' : 'AM'; 
+
+
+hours = hours % 12;
+hours = hours ? hours : 12;
+
+minutes = minutes < 10 ? '0' + minutes : minutes;
+seconds = seconds < 10 ? '0' + seconds : seconds;
+
+ let timeString = hours + ':' + minutes + ':' + seconds;
+ let ampmString = ampm;
+
+document.getElementById("time").innerHTML = timeString;
+document.getElementById("ampm").innerHTML = ampmString;
+
+setTimeout(getTime, 1000);
+}
+
+function setAlarm() {
+ let alarmTime = document.getElementById("alarmTime").value;
+
+if (alarmTime) {
+  let now = new Date();
+  let alarm = new Date(now.toDateString() + " " + alarmTime);
+
+  let difference = alarm - now;
+
+  if (difference < 0) {
+   difference += 24 * 60 * 60 * 1000; 
+  }
+
+  setTimeout(function() {
+    alert("¡ despierta... ya es hora !");
+  }, difference);
+ }
+}
